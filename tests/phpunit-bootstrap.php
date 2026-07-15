8316cf21ca937a1c9bb61c04199fac05fdd28d07
Henning <leutz@pcsg.de>
2026-07-15 13:16:46 +0200

    test: cover cancellation request workflows
    Exercise area flags, request validation, persistence, mail generation, and escaping.
    Initialize the module schema only when required by the integration environment.

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -16,3 +16,41 @@ QUI\Autoloader::$ComposerLoader?->addPsr4(
     'QUI\\ERP\\Order\\CancellationPolicy\\',
     dirname(__DIR__) . '/src/QUI/ERP/Order/CancellationPolicy'
 );
+
+$SchemaManager = QUI::getSchemaManager();
+$areasTable = QUI::getDBTableName('areas');
+$requestsTable = QUI::getDBTableName('requests');
+$areasTableExists = $SchemaManager->tablesExist([$areasTable]);
+
+if (!$areasTableExists) {
+    QUI\Update::importDatabase(OPT_DIR . 'quiqqer/areas/database.xml');
+}
+
+$requiresDatabaseImport = !$SchemaManager->tablesExist([$requestsTable]);
+
+if (!$requiresDatabaseImport) {
+    $areasColumns = $SchemaManager->listTableColumns($areasTable);
+    $requiresDatabaseImport = !isset($areasColumns['ocp']);
+}
+
+if ($requiresDatabaseImport) {
+    QUI\Update::importDatabase(dirname(__DIR__) . '/database.xml');
+}
+
+$areasCount = (int)QUI::getQueryBuilder()
+    ->select('COUNT(*)')
+    ->from(QUI\Utils\Doctrine::quoteIdentifier($areasTable))
+    ->executeQuery()
+    ->fetchOne();
+
+if ($areasCount === 0) {
+    $PermissionUser = new ReflectionProperty(QUI\Permissions\Permission::class, 'User');
+    $previousPermissionUser = $PermissionUser->getValue();
+    QUI\Permissions\Permission::setUser(QUI::getUsers()->getSystemUser());
+
+    try {
+        QUI\ERP\Areas\Import::importPreconfigureAreas('DigitalGoodsFromEuropaToEuropa.xml');
+    } finally {
+        $PermissionUser->setValue(null, $previousPermissionUser);
+    }
+}
